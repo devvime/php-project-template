06e11e320c32bdb6e0f118df12846d4b367470d1
devvime <dihapp@outlook.com>
2026-02-04 00:34:37 -0300

    fix: adjust view

--- src/views/pages/auth/recover_password.php
+++ src/views/pages/auth/recover_password.php
@@ -1,7 +1,7 @@
 <div class="recover-password-form" x-data="recover_password">
   <div class="card">
     <div class="card-body">
-      <h3>Register</h3>
+      <h3>Recover password</h3>
       <input type="email" placeholder="Email..." class="form-control mb-2" x-model="recoverPasswordForm.email" />
       <button class="btn btn-success" @click="handleRecoverPassword">Recover password</button>
       <button class="btn btn-primary" @click="currentForm = 'login'">Back</button>
